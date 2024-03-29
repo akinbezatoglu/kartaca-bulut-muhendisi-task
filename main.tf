d3e451c29fbb4d0f1a220614c117dfefebe13317
akinbezatoglu <akinbezatoglu@gmail.com>
2024-03-29 20:17:15 +0300

    Add resource policy resource to take snaphots

--- main.tf
+++ main.tf
@@ -37,3 +37,53 @@ resource "google_compute_subnetwork" "subnet" {
     }
   }
 }
+
+# Create a snapshot policy
+locals {
+  hourly_schedule = lookup(var.snapshot_schedule, "hourly_schedule", null) == null ? [] : [var.snapshot_schedule["hourly_schedule"]]
+  daily_schedule  = lookup(var.snapshot_schedule, "daily_schedule", null) == null ? [] : [var.snapshot_schedule["daily_schedule"]]
+  weekly_schedule = lookup(var.snapshot_schedule, "weekly_schedule", null) == null ? [] : [var.snapshot_schedule["weekly_schedule"]]
+}
+
+resource "google_compute_resource_policy" "snapshot_policy" {
+  name = var.snapshot_policy_name
+
+  snapshot_schedule_policy {
+    schedule {
+      dynamic "hourly_schedule" {
+        for_each = local.hourly_schedule
+        content {
+          start_time     = hourly_schedule.value["start_time"]
+          hours_in_cycle = hourly_schedule.value["hours_in_cycle"]
+        }
+      }
+      dynamic "daily_schedule" {
+        for_each = local.daily_schedule
+        content {
+          days_in_cycle = 1
+          start_time    = daily_schedule.value["start_time"]
+        }
+      }
+      dynamic "weekly_schedule" {
+        for_each = local.weekly_schedule
+        content {
+          dynamic "day_of_weeks" {
+            for_each = weekly_schedule.value["day_of_weeks"]
+            content {
+              day        = day_of_weeks.value["day"]
+              start_time = day_of_weeks.value["start_time"]
+            }
+          }
+        }
+      }
+    }
+
+    retention_policy {
+      max_retention_days = var.snapshots_retention_days
+    }
+
+    snapshot_properties {
+      storage_locations = [var.snapshots_storage_location]
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -62,3 +62,41 @@ variable "gke_service_secondary_range" {
     ip_cidr_range = "10.100.64.0/20"
   }
 }
+
+variable "snapshot_policy_name" {
+  type    = string
+  default = "autosnap"
+}
+
+variable "snapshot_schedule" {
+  type = object({
+    hourly_schedule = optional(object({
+      start_time     = string
+      hours_in_cycle = string
+    }))
+    daily_schedule = optional(object({
+      start_time = string
+    }))
+    weekly_schedule = optional(object({
+      day_of_weeks = list(object({
+        day        = string
+        start_time = string
+      }))
+    }))
+  })
+  default = {
+    daily_schedule = {
+      start_time = "03:00"
+    }
+  }
+}
+
+variable "snapshots_retention_days" {
+  type    = number
+  default = 7
+}
+
+variable "snapshots_storage_location" {
+  type    = string
+  default = "eu"
+}
