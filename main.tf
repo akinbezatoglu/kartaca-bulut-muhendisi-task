f50c39fcaabdf665c72d8a69014ac00bfa4ae624
akinbezatoglu <akinbezatoglu@gmail.com>
2024-03-29 19:26:51 +0300

    Add network resources

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "random_string" "suffix" {
-  length  = var.suffix_length
+  length = var.suffix_length
 
   # Not included upper and special characters for a clean suffix
   upper   = false
@@ -8,5 +8,32 @@ resource "random_string" "suffix" {
   # This will keep the same random suffix throughout the project.
   keepers = {
     project = var.project
+  }
+}
+
+# Create a VPC Network
+resource "google_compute_network" "network" {
+  name                            = "network-${random_string.suffix.result}"
+  auto_create_subnetworks         = var.disable_auto_create_subnetworks
+  delete_default_routes_on_create = var.enable_delete_default_routes_on_create
+}
+
+locals {
+  secondary_ip_ranges = [var.gke_pod_secondary_range, var.gke_service_secondary_range]
+}
+
+# Subnet configuration
+resource "google_compute_subnetwork" "subnet" {
+  name                     = "subnetwork-${random_string.suffix.result}"
+  network                  = google_compute_network.network.self_link
+  private_ip_google_access = var.enable_private_google_access
+  ip_cidr_range            = var.primary_ip_range
+
+  dynamic "secondary_ip_range" {
+    for_each = local.secondary_ip_ranges
+    content {
+      range_name    = secondary_ip_range.value["range_name"]
+      ip_cidr_range = secondary_ip_range.value["ip_cidr_range"]
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,46 @@ variable "suffix_length" {
   type    = number
   default = 6
 }
+
+# VPC
+variable "disable_auto_create_subnetworks" {
+  type    = bool
+  default = false
+}
+
+variable "enable_delete_default_routes_on_create" {
+  type    = bool
+  default = true
+}
+
+variable "enable_private_google_access" {
+  type    = bool
+  default = true
+}
+
+variable "primary_ip_range" {
+  type    = string
+  default = "10.100.80.0/23"
+}
+
+variable "gke_pod_secondary_range" {
+  type = object({
+    range_name    = string
+    ip_cidr_range = string
+  })
+  default = {
+    range_name    = "pod"
+    ip_cidr_range = "10.100.0.0/18"
+  }
+}
+
+variable "gke_service_secondary_range" {
+  type = object({
+    range_name    = string
+    ip_cidr_range = string
+  })
+  default = {
+    range_name    = "service"
+    ip_cidr_range = "10.100.64.0/20"
+  }
+}
