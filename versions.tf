399a30d0d8db850de47ab11b5d8743f56d6b437c
akinbezatoglu <akinbezatoglu@gmail.com>
2024-04-01 21:49:31 +0300

    fix: destroy calls always appear to fail
    see the solution:
    https://github.com/hashicorp/terraform-provider-google/issues/16275#issuecomment-1825752152

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>4"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.6.0"
